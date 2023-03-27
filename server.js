205e129387a055969aee8d15a0308a99dad2d8f6
Dasha Burgos <dasha.lewis@gmail.com>
2023-03-27 16:36:49 -0400

    created basline for application

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+
+
 const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
 
-  response.send('test request received')
+  response.send('test request received');
 
-})
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
