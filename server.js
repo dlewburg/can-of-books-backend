48acd025b9bd41ff132aa17484392f30e3d90260
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-03-29 12:20:21 -0700

    added in draft code following along with class demo

--- server.js
+++ server.js
@@ -44,6 +44,31 @@ app.delete('/books/:bookID', deleteBooks);
 
 app.post('/books', postBooks);
 
+// Endpoint to Update books
+
+// app.put('/cats/:catID', updateBooks;
+
+// async function updateBooks(request, response, next){
+//   try {
+//     let id = request.params.bookID;
+//     let data = request.body;
+
+//     // ! 3 args
+//     // ! 1st - is the id
+//     // ! 2nd - data
+//     // ! 3rd - options object { new: true, overwrite: true }
+
+//     const updatedBook = await Book.findByIdAndUpdate(id, data, {new: true, overwrite: true } );
+
+//     response.status(200).send(updatedBook);
+
+//     //ID - the cat to update, DATA - the information to update the book with
+//   } catch (error) {
+//     next (error);
+//   }
+// }
+
+
 
 app.get('*', (request, response) => {
   response.status(404).send('Not availabe');
