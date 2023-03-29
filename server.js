b8d830dedb502c86f006d173b8ac8c5861fa7d43
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-03-29 16:18:18 -0700

    adding update book function

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const getBooks = require('./modules/getBooks');
 const postBooks = require('./modules/postBooks');
 const deleteBooks = require('./modules/deleteBooks');
+const updateBooks = require('./modules/updateBooks');
 
 const app = express();
 app.use(cors());
@@ -44,30 +45,10 @@ app.delete('/books/:bookID', deleteBooks);
 
 app.post('/books', postBooks);
 
+
 // Endpoint to Update books
 
-// app.put('/cats/:catID', updateBooks;
-
-// async function updateBooks(request, response, next){
-//   try {
-//     let id = request.params.bookID;
-//     let data = request.body;
-
-//     // ! 3 args
-//     // ! 1st - is the id
-//     // ! 2nd - data
-//     // ! 3rd - options object { new: true, overwrite: true }
-
-//     const updatedBook = await Book.findByIdAndUpdate(id, data, {new: true, overwrite: true } );
-
-//     response.status(200).send(updatedBook);
-
-//     //ID - the cat to update, DATA - the information to update the book with
-//   } catch (error) {
-//     next (error);
-//   }
-// }
-
+app.put('/books/:bookID', updateBooks);
 
 
 app.get('*', (request, response) => {
@@ -81,55 +62,8 @@ app.use((error, request, response, next) => {
 });
 
 
-/*
-Endpoint to delete a book from the database
-!! must have path parameter
-!! path parameter is going to be set with a variable to capture the ID
-!!use the ':' to signify that is it a path parameter
 
-
-
-
-**** LOOKING FOR AXIOS DELETE **** //ID will be the books ID
-
-async function deleteBook(request, response, next) {
-  try{
-
-
-    let id = request.params.catID //path parameter takes in a value and it must be give in url after endpoint
-
-
-    await Book.findByIdAndDelete(id);
-
-
-    response.status(204).send('Cat Deleted')
-
-
-  }catch(error){
-    next(error);
-  }
-}
-
-
-***** ADDING BOOKS BACK ENDPOINT******
-
-app.post('/books', postBook); // body is used for posting -- built with JSON and put everything in quotes
-
-*** put in module *** request.body needs more middleware -- put in "app.use(express.json());" at the top of the page right below the cors**************BRING IT IN
-
-async function postBook (request, response, next) {
-
-  try{
-
-    let createdBook = await Book.create(request.body)
-
-
-    response.status(201).send(createdBook)
-  } catch (error){
-    next(error)
-  }
-}
-
-
-
-*/
+// Endpoint to delete a book from the database
+// !! must have path parameter
+// !! path parameter is going to be set with a variable to capture the ID
+// !!use the ':' to signify that is it a path parameter
